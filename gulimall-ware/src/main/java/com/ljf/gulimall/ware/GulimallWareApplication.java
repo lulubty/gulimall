ccaff484e0d72511fe3243bfac91909ba87cc130
lulubty <861624779@qq.com>
2022-09-01 20:14:39 +0800

    项目初始结构创建

---
+++ gulimall-ware/src/main/java/com/ljf/gulimall/ware/GulimallWareApplication.java
@@ -0,0 +1,13 @@
+package com.ljf.gulimall.ware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GulimallWareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimallWareApplication.class, args);
+    }
+
+}
